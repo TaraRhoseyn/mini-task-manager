45689ee7f9d386840c425a1a3bacbab04e2df68f
TaraRhoseyn <taragulwell@gmail.com>
2021-10-13 14:00:30 +0000

    Insert remaining os environs for starting Flask app

--- app.py
+++ app.py
@@ -6,3 +6,9 @@ if os.path.exists("env.py"):
 # installing instance of Flask:
 app = Flask(__name__)
 
+
+# telling app how & where to run app
+if __name__ == "__main__":
+    app.run(host=os.environ.get("IP"),
+            port=int(os.environ.get("PORT")),
+            debug=True)
